d59922fc41245c5f4d30dbd9ac78d2a2aa9b377b
RobertJing <jack.jing7@gmail.com>
2015-10-15 15:38:03 -0400

    initial trying, not bad

--- js/app.js
+++ js/app.js
@@ -1,8 +1,24 @@
-angular
-    .module('myApp', [])
-    .controller('myController', myController);
-
-function myController() {
-    var vm = this;
-    vm.nums = [1,2,3,4,5,6,7,8,9,10];
-}
+var demo = new Vue({
+    el: '#demo',
+    data: {
+        title: 'todos',
+        todos: [
+            {
+                done: true,
+                content: 'Learn JavaScript'
+            },
+            {
+                done: false,
+                content: 'Learn vue.js'
+            },
+            {
+                done: true,
+                content: 'ni da ye'
+            },
+            {
+                done: false,
+                content: 'hehe'
+            }
+        ]
+    }
+})
